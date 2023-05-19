0a166e88f24845ff769b6d6d38a674a31643337b
laura.souza <laura.souza@gclaims.com.br>
2023-05-18 21:18:11 -0300

    create too components

--- angular-unit-test/src/app/app.module.ts
+++ angular-unit-test/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './components/info/info.component';
+
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: 'info', component: InfoComponent },
+  { path: '', redirectTo: '/menu', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
