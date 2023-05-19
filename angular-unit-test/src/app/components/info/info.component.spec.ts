0ed61812a3118256df2f41e27a391dbb3b36b730
laura.souza <laura.souza@gclaims.com.br>
2023-05-18 22:17:49 -0300

    tests

--- angular-unit-test/src/app/components/info/info.component.spec.ts
+++ angular-unit-test/src/app/components/info/info.component.spec.ts
@@ -1,23 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InfoComponent } from './info.component';
+import { By } from '@angular/platform-browser';
 
 describe('InfoComponent', () => {
   let component: InfoComponent;
   let fixture: ComponentFixture<InfoComponent>;
 
+  //essa função será executada antes de cada um dos blocos de testes
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ InfoComponent ]
     })
-    .compileComponents();
 
+    /**
+     * TestBed é o Angular TesteBed, que é responsável pela configuração dos ambientes de teste, como nosso componente 
+     * não há dependências nós apenas informamos em declarations o componente que vamos testar.
+     */
     fixture = TestBed.createComponent(InfoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  //criamos o teste
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- angular-unit-test/src/app/components/info/info.component.ts
+++ angular-unit-test/src/app/components/info/info.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit {
 
+  list: any = [];
+
+  ngOnInit(): void {
+    this.list.push(
+      {
+        name: 'Dart Vader'
+      },
+      {
+        name: 'Luke Skywalker'
+      },
+      {
+        name: 'Leia'
+      }
+    )
+  }
+  
 }
